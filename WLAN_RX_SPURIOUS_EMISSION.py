c2ada4ba09ee7f9f6794b1d2f791b7a4a8ce9e0b
santhoshreddym <santhoshreddym@fb.com>
2021-04-05 03:14:15 -0700

    rx changes

--- WLAN_RX_SPURIOUS_EMISSION.py
+++ WLAN_RX_SPURIOUS_EMISSION.py
@@ -996,7 +996,7 @@ try:
                     df=pd.DataFrame(dict,index=[0])
                     timestr = time.strftime("%Y%m%d-%H%M%S")
 
-                    df.to_csv('{3}\\CMW_output_{0}_std1{1}_{2}.csv'.format(RAT_info, standard, timestr, mydir_d))
+                    df.to_csv('{3}\\CMW_output_{0}_std1{1}_{2}.csv'.format(RAT_info, std1, timestr, mydir_d))
 
                     rx_per = True
                     if rx_per:
